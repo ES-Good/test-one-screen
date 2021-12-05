03d69d3f24bcfe7d7b36122b1f38264fb322c9da
ES-Good <46601455+ES-Good@users.noreply.github.com>
2021-12-05 22:47:06 +0400

    't'

---
+++ assets/build/js/main.js
@@ -0,0 +1,22 @@
+// Импортируем другие js-файлы
+const btnMenu = document.getElementById('btn-menu')
+const menu = document.getElementById('top-menu')
+const overlay = document.querySelector('.overlay')
+
+btnMenu.onclick = function () {
+    btnMenu.classList.toggle('btn-burger_active')
+    menu.classList.toggle('menu_show')
+    overlay.classList.toggle('overlay_active')
+    setTimeout(() => {
+        overlay.classList.toggle('overlay_show')
+    }, 100);
+}
+
+overlay.onclick = function () {
+    btnMenu.classList.remove('btn-burger_active')
+    menu.classList.remove('menu_show')
+    overlay.classList.remove('overlay_active')
+    setTimeout(() => {
+        overlay.classList.remove('overlay_show')
+    }, 100);
+}
